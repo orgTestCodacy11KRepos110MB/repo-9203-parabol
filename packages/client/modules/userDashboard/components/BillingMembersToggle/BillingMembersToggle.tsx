39b0ecdc9177e1cb2db823c888d5a5fb0f9007c9
gitstart-parabol <parabol@gitstart.com>
2023-01-31 06:43:03 +0000

    feat: SAML UI: Add UI with disabled state

--- packages/client/modules/userDashboard/components/BillingMembersToggle/BillingMembersToggle.tsx
+++ packages/client/modules/userDashboard/components/BillingMembersToggle/BillingMembersToggle.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 import {matchPath} from 'react-router-dom'
 import ToggleNav from '../../../../components/ToggleNav/ToggleNav'
 import useRouter from '../../../../hooks/useRouter'
-import {BILLING_PAGE, MEMBERS_PAGE} from '../../../../utils/constants'
+import {AUTHENTICATION_PAGE, BILLING_PAGE, MEMBERS_PAGE} from '../../../../utils/constants'
 
 interface Props {
   orgId: string
+  featureFlags?: {
+    teamsLimit: boolean
+    SAMLUI: boolean
+  } | null
 }
 const BillingMembersToggle = (props: Props) => {
   const {
@@ -13,9 +17,10 @@ const BillingMembersToggle = (props: Props) => {
     location: {pathname},
     match
   } = useRouter()
-  const {orgId} = props
+  const {orgId, featureFlags} = props
   const areaMatch = matchPath<{area: string}>(pathname, {path: `${match.url}/:area?`})
   const activeOrgDetail = areaMatch?.params.area ?? BILLING_PAGE
+  const SAMLUI = featureFlags?.SAMLUI
   const items = [
     {
       label: 'Billing',
@@ -28,10 +33,18 @@ const BillingMembersToggle = (props: Props) => {
       icon: 'group' as const,
       isActive: activeOrgDetail === MEMBERS_PAGE,
       onClick: () => history.push(`/me/organizations/${orgId}/${MEMBERS_PAGE}`)
+    },
+    {
+      label: 'Authentication',
+      icon: 'key' as const,
+      isActive: activeOrgDetail === AUTHENTICATION_PAGE,
+      onClick: () => history.push(`/me/organizations/${orgId}/${AUTHENTICATION_PAGE}`)
     }
   ]
 
-  return <ToggleNav items={items} />
+  const tabs = SAMLUI ? items : items.slice(0, 2)
+
+  return <ToggleNav items={tabs} />
 }
 
 export default BillingMembersToggle
